b10d1bb28473f3083bbb2bc74f3aaa470c0c8fca
Macrosxd <118136045+Macrosxd@users.noreply.github.com>
2022-11-13 20:16:34 -0400

    Add files via upload

---
+++ ACTIVIDAD.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Actividad_Programacion{
+    public static void main(String args[]){
+Scanner lectoor=new Scanner (System. in) ;
+       String cadena = "";
+       char [] Arraycadena;
+       char caracter;
+       int contador = 0;
+       System.out.println("Inserte una oracion");
+       cadena = lectoor.nextLine();
+       Arraycadena = cadena.toCharArray();
+       for (int i = 0; i < Arraycadena.length; i++){
+           caracter = Arraycadena[i];
+           for (int k = 0; k < Arraycadena.length; k++){
+               if (Arraycadena[k] == caracter) {
+                   contador++;
+               }
+               
+           }
+           System.out.println("La letra "+Arraycadena[i] +" sale "+ contador+" vez");
+           contador = 0;
+       }
+       
+    }
+}
